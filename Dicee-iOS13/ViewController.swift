f384e481e347d6eba5400d6a0761a88b9da74639
brndalx <113251192+brandalx@users.noreply.github.com>
2024-02-12 14:13:30 -0500

    app's logic

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+//    var leftDiceNumber = 0
+//    var rightDiceNumber = 5
+    
+    let data = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageViewOne.image = data.randomElement()
+        
+        diceImageViewTwo.image = data[Int.random(in: 0...5)]
+
+//        leftDiceNumber=leftDiceNumber+1
+//        rightDiceNumber=rightDiceNumber-1
+        
+   
+     
+//        print("leftDiceNumber in the end = \(leftDiceNumber)" )
+//        print("rightDiceNumber in the end = \(rightDiceNumber)" )
+        
+        
+        
+
     }
-
-
+    
 }
 
+
+//        print("Button got tapped")
+//        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
